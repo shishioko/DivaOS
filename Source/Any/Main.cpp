3b1ff3945b5a084a9cc87dcffbd56308968fbdf9
shiokawaii <shishioko@proton.me>
2026-09-17 18:39:10 +0200

    implemented c++

--- Source/Any/Main.cpp
+++ Source/Any/Main.cpp
@@ -1,7 +1,8 @@
-int Main(void)
+extern "C" void Main(void)
 {
     *( (volatile int *) 0x00000000000B8000 ) = 'G';
     *( (volatile int *) 0x00000000000B8001 ) = 0x04;
-    char* t = "welcome to c";
+    const char* t = "welcome to c";
 	repeat: 	goto repeat;
+    return;
 }
