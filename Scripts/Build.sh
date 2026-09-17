186fd625b490fbbacd86d4c385f766f7340f5f9f
shiokawaii <shishioko@proton.me>
2026-09-17 17:30:09 +0200

    terminology

--- Scripts/Build.sh
+++ Scripts/Build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nasm -i ./Source/x86_64/ -i ./Source/Any/ ./Source/Any/Main.asm -f bin -o ./Artifacts/Disk.bin
+nasm -i ./Source/x86_64/ -i ./Source/Any/ ./Source/Any/Boot.asm -f bin -o ./Artifacts/Disk.bin
