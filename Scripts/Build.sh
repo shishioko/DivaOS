9d891dc9e2978bd1fa8f422e5889080ac0c702ec
shiokawaii <shishioko@proton.me>
2026-09-17 18:22:17 +0200

    implement c stuff

--- Scripts/Build.sh
+++ Scripts/Build.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-nasm -i ./Source/x86_64/ -i ./Source/Any/ ./Source/Any/Boot.asm -f bin -o ./Artifacts/Disk.bin
+
+#Create required directories
+mkdir ./{Source,Build,Artifacts}
+
+#Assemble and compile code
+nasm -f elf64 -o ./Build/Loader.o -i ./Source/x86_64/ -i ./Source/Any/ ./Source/Any/Boot.asm
+gcc -ffreestanding -c -o ./Build/Kernel.o -I ./Source/x86_64/ -I ./Source/Any/ ./Source/Any/Main.c
+
+#Produce disk binary
+ld -T ./Scripts/Linker.ld --oformat binary -o ./Artifacts/Disk.bin ./Build/Loader.o ./Build/Kernel.o
