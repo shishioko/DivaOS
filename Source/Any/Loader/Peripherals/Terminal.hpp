e6e15bceaf15bab59a9ec01ccdae84dc7a77153f
shiokawaii <shishioko@proton.me>
2026-09-19 21:50:19 +0200

    idk

--- Source/Any/Loader/Peripherals/Terminal.hpp
+++ Source/Any/Loader/Peripherals/Terminal.hpp
@@ -6,7 +6,6 @@ namespace DivaOS::Loader::Peripherals::Terminal {
     extern void Write(const t8 character) asm("DivaOS.Loader.Peripherals.Terminal.Write_t8");
 
     extern inline void WriteLine(const t8* text) asm("DivaOS.Loader.Peripherals.Terminal.WriteLine_t8p");
-
     inline void WriteLine(const t8* text){
         Write(text);
         Write('\n');
