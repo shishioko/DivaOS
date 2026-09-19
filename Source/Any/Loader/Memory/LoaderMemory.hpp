3b5101b38ab2b476b4deec58a7c7512c36ee7650
shiokawaii <shishioko@proton.me>
2026-09-19 21:47:58 +0200

    fix wrong exported signed name

--- Source/Any/Loader/Memory/LoaderMemory.hpp
+++ Source/Any/Loader/Memory/LoaderMemory.hpp
@@ -2,5 +2,5 @@
 #include "Definitions.hpp"
 
 namespace DivaOS::Loader::Memory::LoaderMemory {
-    extern void* Acquire(const u64 size, const u8 alignment = 0) asm("DivaOS.Loader.Memory.LoaderMemory.Acquire_u64_u1");
+    extern void* Acquire(const u64 size, const u8 alignment = 0) asm("DivaOS.Loader.Memory.LoaderMemory.Acquire_u64_u8");
 }
